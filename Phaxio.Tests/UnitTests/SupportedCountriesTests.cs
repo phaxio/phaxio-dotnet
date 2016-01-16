2c261b60bef095b9f89befd6bcd0f6c9e6d6e689
Noel Herrick <noelherrick@gmail.com>
2016-01-16 00:27:16 -0700

    Added missing files.

---
+++ Phaxio.Tests/UnitTests/SupportedCountriesTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Phaxio.Tests.Fixtures;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phaxio.Tests.UnitTests
+{
+    [TestFixture]
+    public class SupportedCountriesTests
+    {
+        [Test]
+        public void UnitTests_SupportedCountriesRequestWorks()
+        {
+            var clientBuilder = new IRestClientBuilder { Op = "supportedCountries", NoAuth = true };
+
+            var phaxio = new Phaxio(IRestClientBuilder.TEST_KEY, IRestClientBuilder.TEST_SECRET, clientBuilder.Build());
+
+            var countries = phaxio.GetSupportedCountries();
+
+            var expectedCountries = PocoFixtures.GetTestSupporteCountries();
+
+            Assert.AreEqual(expectedCountries.Count(), countries.Count(), "Number should be the same");
+            Assert.AreEqual(expectedCountries["Canada"].PricePerPage, countries["Canada"].PricePerPage, "PricePerPage should be the same");
+        }
+    }
+}
---
+++ Phaxio/Entities/Pricing.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phaxio.Entities
+{
+    public class Pricing
+    {
+        [DeserializeAs(Name = "price_per_page")]
+        public int PricePerPage {get;set;}
+    }
+}
