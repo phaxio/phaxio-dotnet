7ced49e775f63f4fb962328e51a6e644afec7d9c
Noel Herrick <noelherrick@gmail.com>
2016-01-16 00:01:29 -0700

    Added AreaCodesTest (somehow that wasn't added to git in VS)

---
+++ Phaxio.Tests/UnitTests/AreaCodesTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using Phaxio.Tests.Fixtures;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phaxio.Tests.UnitTests
+{
+    [TestFixture]
+    public class AreaCodesTests
+    {
+        [Test]
+        public void UnitTests_AreaCodesRequestWorks()
+        {
+            var clientBuilder = new IRestClientBuilder { Op = "areaCodes", NoAuth = true };
+
+            var phaxio = new Phaxio(IRestClientBuilder.TEST_KEY, IRestClientBuilder.TEST_SECRET, clientBuilder.Build());
+
+            var codes = phaxio.GetAreaCodes(tollFree: true);
+
+            var expectedCodes = PocoFixtures.GetTestAreaCodes();
+
+            Assert.AreEqual(expectedCodes.Count(), codes.Count(), "Number should be the same");
+            Assert.AreEqual(expectedCodes["201"].City, codes["201"].City, "City should be the same");
+            Assert.AreEqual(expectedCodes["201"].State, codes["201"].State, "State should be the same");
+        }
+
+        [Test]
+        public void UnitTests_AreaCodesWithTollFreeRequestWorks()
+        {
+            Action<IRestRequest> requestAsserts = req =>
+            {
+                Assert.AreEqual(req.Parameters[0].Value, true);
+            };
+
+            var clientBuilder = new IRestClientBuilder { Op = "areaCodes", NoAuth = true, RequestAsserts = requestAsserts };
+
+            var phaxio = new Phaxio(IRestClientBuilder.TEST_KEY, IRestClientBuilder.TEST_SECRET, clientBuilder.Build());
+
+            var codes = phaxio.GetAreaCodes(tollFree: true);
+        }
+
+        [Test]
+        public void UnitTests_AreaCodesWithStateRequestWorks()
+        {
+            Action<IRestRequest> requestAsserts = req =>
+            {
+                Assert.AreEqual(req.Parameters[0].Value, "ID");
+            };
+
+            var clientBuilder = new IRestClientBuilder { Op = "areaCodes", NoAuth = true, RequestAsserts = requestAsserts };
+
+            var phaxio = new Phaxio(IRestClientBuilder.TEST_KEY, IRestClientBuilder.TEST_SECRET, clientBuilder.Build());
+
+            var codes = phaxio.GetAreaCodes(state: "ID");
+        }
+    }
+}
